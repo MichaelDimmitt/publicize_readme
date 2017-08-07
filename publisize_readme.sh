27a7878c142c8b9c14277155d43ef1239b954260
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-07 19:35:26 -0400

    publicize_readme.sh: push core script.

---
+++ publisize_readme.sh
@@ -0,0 +1,14 @@
+mkdir publicize_readme;
+cp Readme* publicize_readme;
+cd publicize_readme;
+git init;
+git add Readme*;
+git remote add publicize_readme "https://gitlab.com/$1/$2";
+# git remote add publicize_readme "https://gitlab.com/general_organization/$1";
+git push publicize_readme master; 
+# git push "https://gitlab.com/general_organization/$1";
+# git push "https://gitlab.com/$1/$2";
+
+cd ..;
+rm -rf publicize_readme;
+git remove publicize_readme;
