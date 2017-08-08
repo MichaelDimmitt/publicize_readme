8f21966321a02ead49d6e4fbb02cabe777e1d21d
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-08 08:17:43 -0400

    publicize_readme.sh: fix issue with remote rm command and bash logic

--- publicize_readme.sh
+++ publicize_readme.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # mkdir publicize_readme;cp Readme* publicize_readme;cd publicize_readme;git init;git add Readme*;git remote add publicize_readme "https://gitlab.com/$1/$2";git push publicize_readme master; cd ..;rm -rf publicize_readme;git remove publicize_readme;
 
 mkdir publicize_readme;
@@ -11,7 +11,6 @@ git remote add publicize_readme "https://gitlab.com/$1/$2";
 git push publicize_readme master; 
 # git push "https://gitlab.com/general_organization/$1";
 # git push "https://gitlab.com/$1/$2";
-
+git remote rm publicize_readme;
 cd ..;
 rm -rf publicize_readme;
-git remove publicize_readme;
