4b63da850d610757d170d5bb7d3c722dddbe97a5
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-08 18:46:44 -0400

    publicize_readme.sh: cleanup, removing some code.

--- publicize_readme.sh
+++ publicize_readme.sh
@@ -2,15 +2,17 @@
 # mkdir publicize_readme;cp Readme* publicize_readme;cd publicize_readme;git init;git add Readme*;git remote add publicize_readme "https://gitlab.com/$1/$2";git push publicize_readme master; cd ..;rm -rf publicize_readme;git remove publicize_readme;
 
 mkdir publicize_readme;
-cp Readme* publicize_readme;
+cp Readme.md publicize_readme;
+
 cd publicize_readme;
 git init;
-git add Readme*;
-git remote add publicize_readme "https://gitlab.com/$1/$2";
-# git remote add publicize_readme "https://gitlab.com/general_organization/$1";
-git push publicize_readme master; 
-# git push "https://gitlab.com/general_organization/$1";
-# git push "https://gitlab.com/$1/$2";
+git add Readme.md; 
+# git add public_folder #package.*;
+
+
+git remote add publicize_readme "https://gitlab.com/general_organization/$1";
+git push publicize_readme master;
+
 git remote rm publicize_readme;
 cd ..;
 rm -rf publicize_readme;
